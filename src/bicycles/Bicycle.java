585f7a9a965d59c885454174771d3f30c160ced4
ammaar <fakirammaar8@gmail.com>
2021-04-18 22:19:04 +0200

    started creating methods and importing packages

---
+++ src/bicycles/Bicycle.java
@@ -0,0 +1,21 @@
+package bicycles;
+
+public class Bicycle {
+    private int speed;
+    public void accelerate(){
+       speed=speed+5;
+
+    }
+    public void brake(){
+speed=speed-3;
+
+    }
+    public int currentSpeed(){
+
+return speed;
+    }
+    public void stop(){
+speed=0;
+
+    }
+}
---
+++ tests/BicycleTest/BicycleTest.java
@@ -0,0 +1,8 @@
+package BicycleTest;
+
+public class BicycleTest {
+    @Test
+    public void shouldAccelerate() {
+        assertEquals(1,2);
+    }
+}
