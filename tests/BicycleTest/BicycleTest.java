a8793692bb2d0a5208eba4efd0de3a28b2ff6529
ammaar <fakirammaar8@gmail.com>
2021-04-18 22:20:02 +0200

    made changes

--- tests/BicycleTest/BicycleTest.java
+++ tests/BicycleTest/BicycleTest.java
@@ -1,8 +1,13 @@
 package BicycleTest;
 
+import org.junit.jupiter.api.Test;
+
 public class BicycleTest {
     @Test
     public void shouldAccelerate() {
         assertEquals(1,2);
+    }
+
+    private void assertEquals(int i, int i1) {
     }
 }
