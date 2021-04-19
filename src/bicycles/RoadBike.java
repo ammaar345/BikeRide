66eac3d2cd3518459bfeb5e0b497ffa10871f240
Ammaar <fakirammaar8@gmail.com>
2021-04-19 15:20:16 +0200

    added method logic for different bikes

--- src/bicycles/RoadBike.java
+++ src/bicycles/RoadBike.java
@@ -1,4 +1,24 @@
 package bicycles;
 
-public class RoadBike {
+public class RoadBike implements Bicycle{
+private int speed;
+    @Override
+    public void accelerate() {
+speed=speed+11;
+    }
+
+    @Override
+    public void brake() {
+speed=speed-4;
+    }
+
+    @Override
+    public int currentSpeed() {
+        return speed;
+    }
+
+    @Override
+    public void stop() {
+speed=0;
+    }
 }
