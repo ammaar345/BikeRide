d11170b49056c55f06570fcf2794c69bdcf8b61a
Ammaar <fakirammaar8@gmail.com>
2021-04-19 16:02:09 +0200

    made changes

--- tests/BicycleTest/BikeRideTest.java
+++ tests/BicycleTest/BikeRideTest.java
@@ -1,4 +1,12 @@
 package BicycleTest;
 
+import bicycles.Bicycle;
+import models.RoadBike;
+import org.junit.jupiter.api.Test;
+
 public class BikeRideTest {
+    @Test
+    public void shouldBanana(){
+RoadBike.accelerate();
+    }
 }
