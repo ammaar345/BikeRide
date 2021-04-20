6982a9dc78753cb8386af1a5f6980b942cc95fb2
ammaar <fakirammaar8@gmail.com>
2021-04-20 11:46:16 +0200

    changed code to use the newly declared method that takes a speed inside

--- src/models/MountainBike.java
+++ src/models/MountainBike.java
@@ -1,26 +1,14 @@
 package models;
 
-import models.Bicycle;
-
-public class MountainBike implements Bicycle {
-    private int speed;
+public class MountainBike extends BicycleBase {
     @Override
     public void accelerate() {
-speed=speed+5;
+changeSpeed(+5);
     }
 
     @Override
     public void brake() {
-speed=speed-3;
+changeSpeed(-3);
     }
 
-    @Override
-    public int currentSpeed() {
-        return speed;
-    }
-
-    @Override
-    public void stop() {
-speed=0;
-    }
 }
--- src/models/RoadBike.java
+++ src/models/RoadBike.java
@@ -1,26 +1,14 @@
 package models;
-//
-//import models.Bicycle;
 
-public class RoadBike implements Bicycle {
-private int speed;
+public class RoadBike extends BicycleBase {
     @Override
     public void accelerate() {
-speed=speed+11;
+changeSpeed(+11);
     }
 
     @Override
     public void brake() {
-speed=speed-4;
+changeSpeed(-4);
     }
 
-    @Override
-    public int currentSpeed() {
-        return speed;
-    }
-
-    @Override
-    public void stop() {
-speed=0;
-    }
 }
