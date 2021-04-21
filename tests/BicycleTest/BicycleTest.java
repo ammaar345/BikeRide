d869ef12d0b5970d8fa4e5a698416b0b316fba8d
Ammaar <fakirammaar8@gmail.com>
2021-04-21 11:00:15 +0200

    Created tests for each bike ride , and cleaned up code.

--- tests/BicycleTest/BicycleTest.java
+++ tests/BicycleTest/BicycleTest.java
@@ -1,56 +1,79 @@
 package BicycleTest;
 
-//import bicycles.Bicycle;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import models.MountainBike;
-import models.RoadBike;
-public class BicycleTest {
-//    @Test
-//    public void shouldAccelerate() {
-//
-//        MountainBike mountainBike = new MountainBike();
-//        mountainBike.accelerate();
-//   assertEquals(5, bicycle.currentSpeed());
-//    }
+import models.*;
 
-//    @Test
-//    public void shouldAccelerateAndBrakeCorrect() {
-//        Bicycle bicycle = new Bicycle();
-//        bicycle.accelerate();
-//        bicycle.brake();
-//        assertEquals(bicycle.currentSpeed(), 2);
-//
-//
-//    }
-//
-//    @Test
-//    public void shouldDoMultipleAcceleratesCorrectly() {
-//        Bicycle bicycle = new Bicycle();
-//        bicycle.accelerate();
-//        bicycle.accelerate();
-//        bicycle.accelerate();
-//        assertEquals(bicycle.currentSpeed(), 15);
-//    }
-//@Test
-//    public void shouldDoMultipleAcceleratesAndBrakesCorrectly(){
-//        Bicycle bicycle=new Bicycle();
-//        bicycle.accelerate();
-//        bicycle.brake();
-//        bicycle.accelerate();
-//        bicycle.brake();
-//        bicycle.brake();
-//        assertEquals(bicycle.currentSpeed(),1);
-//}
-//@Test
-//    public void shouldBeAbleToStop(){
-//Bicycle bicycle=new Bicycle();
-//bicycle.accelerate();
-//bicycle.brake();
-//bicycle.accelerate();
-//bicycle.stop();
-//assertEquals(bicycle.currentSpeed(),0);
-//
-//}
+public class BicycleTest {
+    @Test
+    public void shouldTestBikeRideOneMountain() {
+        MountainBike mountainBike = new MountainBike();
+        BikeRideOne bikeRideOne = new BikeRideOne();
+        bikeRideOne.ride(mountainBike);
+        assertEquals(mountainBike.currentSpeed(), 17);
+    }
+
+    @Test
+    public void shouldTestBikeRideOneRoad() {
+        RoadBike roadBike = new RoadBike();
+        BikeRideOne bikeRideOne = new BikeRideOne();
+        bikeRideOne.ride(roadBike);
+        assertEquals(roadBike.currentSpeed(), 40);
+    }
+
+    @Test
+    public void shouldTestBikeRideOneTandem() {
+        Tandem tandem = new Tandem();
+        BikeRideOne bikeRideOne = new BikeRideOne();
+        bikeRideOne.ride(tandem);
+        assertEquals(tandem.currentSpeed(), 41);
+    }
+
+    @Test
+    public void shouldTestBikeRideTwoTandem() {
+        Tandem tandem = new Tandem();
+        BikeRideTwo bikeRideTwo = new BikeRideTwo();
+        bikeRideTwo.ride(tandem);
+        assertEquals(tandem.currentSpeed(), 60);
+    }
+
+    @Test
+    public void shouldTestBikeRideTwoMountain() {
+        MountainBike mountainBike = new MountainBike();
+        BikeRideTwo bikeRideTwo = new BikeRideTwo();
+        bikeRideTwo.ride(mountainBike);
+        assertEquals(mountainBike.currentSpeed(), 25);
+    }
+
+    @Test
+    public void shouldTestBikeRideTwoRoad() {
+        RoadBike roadBike = new RoadBike();
+        BikeRideTwo bikeRideTwo = new BikeRideTwo();
+        bikeRideTwo.ride(roadBike);
+        assertEquals(roadBike.currentSpeed(), 55);
+    }
+
+    @Test
+    public void shouldTestBikeRideThreeMountain() {
+        MountainBike mountainBike = new MountainBike();
+        BikeRideThree bikeRideThree = new BikeRideThree();
+        bikeRideThree.ride(mountainBike);
+        assertEquals(mountainBike.currentSpeed(), 0);
+    }
+
+    @Test
+    public void shouldTestBikeRideThreeRoad() {
+        RoadBike roadBike = new RoadBike();
+        BikeRideThree bikeRideThree = new BikeRideThree();
+        bikeRideThree.ride(roadBike);
+    }
+
+    @Test
+    public void shouldTestBikeRideThreeTandem() {
+        Tandem tandem = new Tandem();
+        BikeRideThree bikeRideThree = new BikeRideThree();
+        bikeRideThree.ride(tandem);
+        assertEquals(tandem.currentSpeed(), 0);
+
+    }
 }
