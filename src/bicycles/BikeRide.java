99ae59a5e571618ae09b13e11a296c86a16bc6dc
Ammaar <fakirammaar8@gmail.com>
2021-04-21 10:58:48 +0200

    Cleaned up code for interface.

--- src/bicycles/BikeRide.java
+++ src/bicycles/BikeRide.java
@@ -1,37 +1,8 @@
 package bicycles;
 
-//import models.Bicycle;
 import models.RoadBike;
 import models.*;
-public interface BikeRide {
- void ride(Bicycle bicycle);
-//    public final Bicycle bicycle;
-//public BikeRide(Bicycle bicycle1){
-//    this.bicycle = bicycle1;
-////    Bicycle= bicycle;
-//
-//
-//}
-//
-//    public void ride(){
-////        this.bicycle.accelerate();
-////        this.bicycle.accelerate();
-////        this.bicycle.accelerate();
-////        this.bicycle.brake();
-////        this.bicycle.brake();
-////        this.bicycle.accelerate();
-//
-//    }
-//
-//    public void currentSpeed(){
-//       System.out.println(this.bicycle.currentSpeed()) ;
-//    }
-//
-//    public static void main(String[] args) {
-////        Bicycle bicycle =new Bicycle();
-////        BikeRide bikeRide=new BikeRide(bicycle);
-////        bikeRide.ride();
-////        System.out.println(bikeRide.currentSpeed());
-//
 
+public interface BikeRide {
+    void ride(Bicycle bicycle);
 }
