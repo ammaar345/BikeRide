5291e2b35358afe51bd0c7ba474f7d8e1e8555fe
Ammaar <fakirammaar8@gmail.com>
2021-04-26 15:17:12 +0200

    Added method getBicycleType.

--- src/bicycles/BicycleSpecification.java
+++ src/bicycles/BicycleSpecification.java
@@ -1,15 +1,20 @@
 package bicycles;
 
 public  class BicycleSpecification {
+
+//    public static Object BicycleType;
+    private BicycleType bicycleType;
     private int accelerationSpeed;
     private int brakeSpeed;
 
-    public BicycleSpecification(int speed, int brake) {
+    public BicycleSpecification(int speed, int brake,BicycleType bicycleType) {
         this.accelerationSpeed = speed;
         this.brakeSpeed = brake;
-
+        this.bicycleType = bicycleType;
     }
-
+    public BicycleType getBicycleType(){
+        return bicycleType;
+    }
     public int getAccelerationSpeed() {
         return accelerationSpeed;
 
@@ -19,4 +24,5 @@ public  class BicycleSpecification {
         return brakeSpeed;
 
     }
+
 }
